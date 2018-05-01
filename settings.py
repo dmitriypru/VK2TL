1dcaf1c2bf21ac36e64110166b64b940027c0df9
iTROLLFACEi <minelive0@gmail.com>
2018-05-01 22:04:48 +0500

    Changed token adn updated requirements

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 class SETTINGS:
     __slots__ = ("TOKEN","DB_NAME")
     def __init__(self):
-        self.TOKEN = '415193750:AAF6ndOpFEhi8MEMmINmUIm8Lj2bWACnhus'
+        self.TOKEN = '415193750:AAFNBxqmF5ow24TwzuJlzYKpYSPmt_K5p_A'
         self.DB_NAME = 'vk2tl.db'
